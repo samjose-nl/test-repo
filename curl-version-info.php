02f1db7fd10f361b764b9e5bef7fb0194f578528
samjose-nl <samjose-nl@users.noreply.github.com>
2015-08-04 14:32:25 +0530

    Create curl-version-info.php

---
+++ curl-version-info.php
@@ -0,0 +1,21 @@
+<?php
+/* The below code will return your cURL verson details if cURL is enabled. */
+// Get curl version array
+$version = curl_version();
+
+// These are the bitfields that can be used 
+// to check for features in the curl build
+$bitfields = Array(
+            'CURL_VERSION_IPV6', 
+            'CURL_VERSION_KERBEROS4', 
+            'CURL_VERSION_SSL', 
+            'CURL_VERSION_LIBZ'
+            );
+
+if(function_exists('curl_version'))echo "CURL is ENABLED<br>";
+foreach($bitfields as $feature)
+{
+    echo $feature . ($version['features'] & constant($feature) ? ' matches' : ' does not match');
+    echo PHP_EOL;
+}
+?>
